dbc7be20dc9d858c48656ffb75a15964a6b0fadf
Matthieu Napoli <matthieu@mnapoli.fr>
2013-05-15 19:39:33 +0200

    Vagrant

--- README.md
+++ README.md
@@ -7,3 +7,23 @@ Install [composer](http://getcomposer.org/doc/00-intro.md) and run:
 ```bash
 $ composer install
 ```
+
+## Run
+
+Install [Virtual Box](https://www.virtualbox.org/wiki/Downloads) and [Vagrant](http://www.vagrantup.com/):
+
+```bash
+$ vagrant up
+```
+
+SSH to the virtual machine:
+
+```bash
+$ vagrant ssh
+```
+
+Destroy the VM:
+
+```bash
+$ vagrant destroy
+```
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -9,12 +9,14 @@ apt-get update
 
 apt-get install -y bash-completion curl lynx
 
-# Shell
-apt-get install -y zsh
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-
 # SCM
 apt-get install -y git
+
+# Shell
+apt-get install -y zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
+chsh -s $(which zsh) vagrant
+cp /vagrant/vagrant/.zshrc /home/vagrant/
 
 # Mysql
 export DEBIAN_FRONTEND=noninteractive
@@ -27,6 +29,7 @@ apt-get install -y php5 php5-curl php5-cli php5-gd php5-mcrypt php5-dev php5-mys
 apt-get install -y apache2
 rm -rf /var/www
 ln -fs /vagrant/public /var/www
+cp /vagrant/vagrant/php.ini /etc/php5/apache2/
 
 # phpMyAdmin
 export DEBIAN_FRONTEND=noninteractive
