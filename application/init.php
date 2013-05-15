23000c6efa4f814a5e30421a6d0db99cdc0619cf
Matthieu Napoli <matthieu@mnapoli.fr>
2013-05-15 18:58:13 +0200

    Init

---
+++ application/init.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Initialisation de l'application
+ */
+
+error_reporting(E_ALL);
+
+// Répertoire vers l'application
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', dirname(__FILE__));
+
+// Répertoire vers le package
+defined('PACKAGE_PATH') || define('PACKAGE_PATH', realpath(APPLICATION_PATH . '/..'));
+
+// Vérifie que l'environnement d'exécution est définit
+if (! defined('APPLICATION_ENV')) {
+    die("Aucun environnement d'exécution défini");
+}
+
+require_once PACKAGE_PATH . '/vendor/autoload.php';
+
+require_once 'Zend/Application.php';
+
+// Config
+$config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV);
+$configShared = new Zend_Config_Ini(APPLICATION_PATH . '/configs/shared.ini', APPLICATION_ENV, true);
+$configShared->merge($config);
+$configShared->setReadOnly();
+
+// Crée l'application
+$application = new Zend_Application(APPLICATION_ENV, $configShared);
+
+// Lance le bootstrap, puis l'application
+$application->bootstrap();
+if (defined('RUN') && RUN) {
+    $application->run();
+}
