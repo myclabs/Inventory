f7e86850f6a09a7e499e1f634cc37ac95c325cb0
Matthieu Napoli <matthieu@mnapoli.fr>
2013-06-11 12:19:56 +0200

    Correction des tests

--- tests/Core/Work/GearmanDispatcherTest.php
+++ tests/Core/Work/GearmanDispatcherTest.php
@@ -5,6 +5,8 @@
  * @subpackage Test
  */
 
+use DI\Container;
+
 /**
  * @package    Core
  * @subpackage Test
@@ -18,7 +20,7 @@ class Core_Test_Work_GearmanDispatcherTest extends PHPUnit_Framework_TestCase
             $this->markTestSkipped('Extension Gearman non installée');
         }
         $dispatcher = new Core_Work_GearmanDispatcher();
-        $dispatcher->registerWorker(new Core_Work_ServiceCall_Worker());
+        $dispatcher->registerWorker(new Core_Work_ServiceCall_Worker(new Container()));
 
         $task = new Core_Work_ServiceCall_Task('Inventory_Service_Test', 'doSomething', ['foo']);
 
