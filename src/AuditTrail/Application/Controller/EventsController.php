04726b0c19ca9b8bdfb92898188f2aaed2f01c47
Matthieu Napoli <matthieu@mnapoli.fr>
2013-06-18 12:51:32 +0200

    ref #5883 Page d'affichage des évènements

---
+++ src/AuditTrail/Application/Controller/EventsController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author  matthieu.napoli
+ * @author  hugo.charbonnier
+ * @author  yoann.croizer
+ * @package AF
+ */
+
+use AuditTrail\Domain\EntryRepository;
+use Core\Annotation\Secure;
+use DI\Annotation\Inject;
+
+/**
+ * Countroleur des AF
+ * @package AF
+ */
+class AuditTrail_EventsController extends Core_Controller
+{
+    /**
+     * @Inject
+     * @var EntryRepository
+     */
+    private $entryRepository;
+
+    /**
+     * Recent events
+     * @Secure("public")
+     */
+    public function recentAction()
+    {
+    }
+}
