6eec1c3535a48b5bfe407c5468cb5291e19caa00
Matthieu Napoli <matthieu@mnapoli.fr>
2013-07-05 15:32:09 +0200

    ref #5881 Remplacer le datagrid des sous-formulaires par deux datagrids

--- application/af/controllers/Datagrid/Edit/Components/SubAfNotRepeatedController.php
+++ application/af/controllers/Datagrid/Edit/Components/SubAfNotRepeatedController.php
@@ -12,7 +12,7 @@ use Core\Annotation\Secure;
  * Conditions Controller
  * @package AF
  */
-class AF_Datagrid_Edit_Components_SubAfController extends UI_Controller_Datagrid
+class AF_Datagrid_Edit_Components_SubAfNotRepeatedController extends UI_Controller_Datagrid
 {
 
     /**
@@ -26,38 +26,21 @@ class AF_Datagrid_Edit_Components_SubAfController extends UI_Controller_Datagrid
         $af = AF_Model_AF::load($this->getParam('id'));
         // Filtre sur l'AF
         $this->request->filter->addCondition(AF_Model_Component::QUERY_AF, $af);
-        /** @var $subAFList AF_Model_Component_SubAF[] */
-        $subAFList = AF_Model_Component_SubAF::loadList($this->request);
+        /** @var $subAFList AF_Model_Component_SubAF_NotRepeated[] */
+        $subAFList = AF_Model_Component_SubAF_NotRepeated::loadList($this->request);
         foreach ($subAFList as $subAF) {
             $data = [];
             $data['index'] = $subAF->getId();
             $data['label'] = $subAF->getLabel();
             $data['ref'] = $subAF->getRef();
             $data['help'] = $this->cellLongText('af/edit_components/popup-help/id/' . $subAF->getId(),
-                                                ' af/datagrid_edit_components_sub-af/get-raw-help/id/'
+                                                ' af/datagrid_edit_components_sub-af-not-repeated/get-raw-help/id/'
                                                     . $subAF->getId(),
                                                 __('UI', 'name', 'help'),
                                                 'zoom-in');
             $data['isVisible'] = $subAF->isVisible();
             $data['targetAF'] = $subAF->getCalledAF()->getId();
             $data['foldaway'] = $subAF->getFoldaway();
-            if ($subAF instanceof AF_Model_Component_SubAF_Repeated) {
-                switch ($subAF->getMinInputNumber()) {
-                    case AF_Model_Component_SubAF_Repeated::MININPUTNUMBER_0:
-                        $data['isRepeated'] = AF_Model_Component_SubAF_Repeated::REPEATED_SUB_AF_NULL;
-                        break;
-                    case AF_Model_Component_SubAF_Repeated::MININPUTNUMBER_1_NOT_DELETABLE:
-                        $data['isRepeated'] = AF_Model_Component_SubAF_Repeated::REPEATED_SUB_AF_ONE_MANDATORY;
-                        break;
-                    case AF_Model_Component_SubAF_Repeated::MININPUTNUMBER_1_DELETABLE:
-                        $data['isRepeated'] = AF_Model_Component_SubAF_Repeated::REPEATED_SUB_AF_ONE_DELETABLE;
-                        break;
-                }
-                $data['hasFreeLabel'] = $subAF->getWithFreeLabel();
-            } else {
-                $data['isRepeated'] = AF_Model_Component_SubAF_Repeated::NOT_REPEATED_SUB_AF;
-                $data['hasFreeLabel'] = 0;
-            }
             $this->addLine($data);
         }
         $this->send();
@@ -80,32 +63,9 @@ class AF_Datagrid_Edit_Components_SubAfController extends UI_Controller_Datagrid
         if (empty($isVisible)) {
             $this->setAddElementErrorMessage('isVisible', __('UI', 'formValidation', 'emptyRequiredField'));
         }
-        $isRepeated = $this->getAddElementValue('isRepeated');
-        if (empty($isRepeated)) {
-            $this->setAddElementErrorMessage('isRepeated', __('UI', 'formValidation', 'emptyRequiredField'));
-        }
         // Pas d'erreurs
         if (empty($this->_addErrorMessages)) {
-            // Repeated / Not repeated
-            switch ($isRepeated) {
-                case AF_Model_Component_SubAF_Repeated::NOT_REPEATED_SUB_AF:
-                    $subAF = new AF_Model_Component_SubAF_NotRepeated();
-                    break;
-                case AF_Model_Component_SubAF_Repeated::REPEATED_SUB_AF_NULL:
-                    $subAF = new AF_Model_Component_SubAF_Repeated();
-                    $subAF->setMinInputNumber(AF_Model_Component_SubAF_Repeated::MININPUTNUMBER_0);
-                    break;
-                case AF_Model_Component_SubAF_Repeated::REPEATED_SUB_AF_ONE_MANDATORY:
-                    $subAF = new AF_Model_Component_SubAF_Repeated();
-                    $subAF->setMinInputNumber(AF_Model_Component_SubAF_Repeated::MININPUTNUMBER_1_NOT_DELETABLE);
-                    break;
-                case AF_Model_Component_SubAF_Repeated::REPEATED_SUB_AF_ONE_DELETABLE:
-                    $subAF = new AF_Model_Component_SubAF_Repeated();
-                    $subAF->setMinInputNumber(AF_Model_Component_SubAF_Repeated::MININPUTNUMBER_1_DELETABLE);
-                    break;
-                default:
-                    throw new Core_Exception("Type de sous-formulaire inconnu");
-            }
+            $subAF = new AF_Model_Component_SubAF_NotRepeated();
             $subAF->setAf($af);
             try {
                 $subAF->setRef($ref);
@@ -121,9 +81,6 @@ class AF_Datagrid_Edit_Components_SubAfController extends UI_Controller_Datagrid
             /** @var $calledAF AF_Model_AF */
             $calledAF = AF_Model_AF::load($this->getAddElementValue('targetAF'));
             $subAF->setCalledAF($calledAF);
-            if ($subAF instanceof AF_Model_Component_SubAF_Repeated) {
-                $subAF->setWithFreeLabel($this->getAddElementValue('hasFreeLabel'));
-            }
             $af->getRootGroup()->addSubComponent($subAF);
             $af->addComponent($subAF);
 
@@ -149,8 +106,8 @@ class AF_Datagrid_Edit_Components_SubAfController extends UI_Controller_Datagrid
      */
     public function updateelementAction()
     {
-        /** @var $subAF AF_Model_Component_SubAF */
-        $subAF = AF_Model_Component_SubAF::load($this->update['index']);
+        /** @var $subAF AF_Model_Component_SubAF_NotRepeated */
+        $subAF = AF_Model_Component_SubAF_NotRepeated::load($this->update['index']);
         $newValue = $this->update['value'];
         switch ($this->update['column']) {
             case 'ref':
@@ -181,14 +138,6 @@ class AF_Datagrid_Edit_Components_SubAfController extends UI_Controller_Datagrid
                 $subAF->setCalledAF(AF_Model_AF::load($newValue));
                 $this->data = $subAF->getCalledAF()->getId();
                 break;
-            case 'hasFreeLabel':
-                if ($subAF instanceof AF_Model_Component_SubAF_Repeated) {
-                    /** @var $subAF AF_Model_Component_SubAF_Repeated */
-                    $subAF->setWithFreeLabel($newValue);
-                } else {
-                    throw new Core_Exception_User('AF', 'configComponentMessage', 'notRepeatedWithoutFreeLAbel');
-                }
-                break;
         }
         $subAF->save();
         $this->entityManager->flush();
@@ -203,8 +152,8 @@ class AF_Datagrid_Edit_Components_SubAfController extends UI_Controller_Datagrid
      */
     public function deleteelementAction()
     {
-        /** @var $subAF AF_Model_Component_SubAF */
-        $subAF = AF_Model_Component_SubAF::load($this->getParam('index'));
+        /** @var $subAF AF_Model_Component_SubAF_NotRepeated */
+        $subAF = AF_Model_Component_SubAF_NotRepeated::load($this->getParam('index'));
         $subAF->delete();
         $this->entityManager->flush();
         $this->message = __('UI', 'message', 'deleted');
@@ -217,8 +166,8 @@ class AF_Datagrid_Edit_Components_SubAfController extends UI_Controller_Datagrid
      */
     public function getRawHelpAction()
     {
-        /** @var $subAF AF_Model_Component_SubAF */
-        $subAF = AF_Model_Component_SubAF::load($this->getParam('id'));
+        /** @var $subAF AF_Model_Component_SubAF_NotRepeated */
+        $subAF = AF_Model_Component_SubAF_NotRepeated::load($this->getParam('id'));
         $this->data = $subAF->getHelp();
         $this->send();
     }
