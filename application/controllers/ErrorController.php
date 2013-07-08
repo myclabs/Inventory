b6ab1489030c44af7a1db2fe56cacb72bafe6550
Matthieu Napoli <matthieu@mnapoli.fr>
2013-07-08 10:39:44 +0200

    Rétablissement erreur 500 en cas d'exception NotFound

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -91,8 +91,8 @@ class ErrorController extends Core_Controller
                     // 404 Not found
                 } elseif ($error->exception instanceof Core_Exception_NotFound) {
                     $log->logException($error->exception);
-                    $errorInfos['code'] = 404;
-                    $errorInfos['message'] = Core_Translate::get('Core', 'exception', 'pageNotFound');
+                    $errorInfos['code'] = 500;
+                    $errorInfos['message'] = Core_Translate::get('Core', 'exception', 'applicationError');
                     // 500 Server error
                 } else {
                     $log->logException($error->exception);
