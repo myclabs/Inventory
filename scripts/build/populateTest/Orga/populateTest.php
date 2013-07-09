7b7ebb54fd9b639331971d83c7fbff5136724758
Emmanuel Risler <emmanuel.risler@myc-sense.com>
2013-07-09 15:30:55 +0200

    Reprise tests behat après scripts Valentin (Début paramétrage organisation).

--- scripts/build/populateTest/Orga/populateTest.php
+++ scripts/build/populateTest/Orga/populateTest.php
@@ -22,48 +22,69 @@ class Orga_PopulateTest extends Core_Script_Action
 
         // Création d'une organisation.
         // Param : label
-        $organization = $this->createOrganization('label');
+        $organization = $this->createOrganization('Organisation avec données');
+        $organization_vide = $this->createOrganization('Organisation vide');
 
         // Création des axes.
         // Params : Organization, ref, label
         // OptionalParams : Axis parent=null
-        $axis1 = $this->createAxis($organization, 'ref1', 'Label 1');
-        $axis11 = $this->createAxis($organization, 'ref11', 'Label 11', $axis1);
-        $axis2 = $this->createAxis($organization, 'ref2', 'Label 11', $axis1);
+        $axis_annee = $this->createAxis($organization, 'annee', 'Année');
+        $axis_site = $this->createAxis($organization, 'site', 'Site');
+        $axis_pays = $this->createAxis($organization, 'pays', 'Pays', $axis_site);
+        $axis_zone = $this->createAxis($organization, 'zone', 'Zone', $axis_pays);
+        $axis_marque = $this->createAxis($organization, 'marque', 'Marque', $axis_site);
+        $axis_categorie = $this->createAxis($organization, 'categorie', 'Catégorie');
+        $axis_vide = $this->createAxis($organization, 'axe_vide', 'Axe vide');
+
 
         // Création des membres.
         // Params : Axis, ref, label
         // OptionalParams : [Member] parents=[]
-        $member11a = $this->createMember($axis11, 'ref11a', 'Label 11 A');
-        $member11b = $this->createMember($axis11, 'ref11b', 'Label 11 B');
-        $member1a = $this->createMember($axis1, 'ref1a', 'Label 1 A', [$member11a]);
-        $member1b = $this->createMember($axis1, 'ref1b', 'Label 1 B', [$member11b]);
-        $member2a = $this->createMember($axis2, 'ref2a', 'Label 2 A');
+        $member_annee_2013 = $this->createMember($axis_annee, '2013', '2013');
+        $member_zone_europe = $this->createMember($axis_zone, 'europe', 'Europe');
+        $member_pays_france = $this->createMember($axis_pays, 'france', 'France', [$member_zone_europe]);
+        $member_marque_marque_a = $this->createMember($axis_marque, 'marque_a', 'Marque A');
+        $member_marque_marque_sans_site = $this->createMember($axis_marque, 'marque_sans_site', 'Marque sans site');
+        $member_site_annecy = $this->createMember($axis_site, 'annecy', 'Annecy', [$member_pays_france, $member_marque_marque_a]);
+        $member_site_chambery = $this->createMember($axis_site, 'chambery', 'Chambery', [$member_pays_france, $member_marque_marque_a]);
+        $member_categorie = $this->createMember($axis_categorie, 'energie', 'Énergie');
 
-        // Création des granularité.
+        // Création des granularités.
         // Params : Organization, axes[Axis], navigable
         // OptionalParams : orgaTab=false, aCL=true, aFTab=false, dWCubes=false, genericAction=false, contextAction=false, inputDocs=false
-        $granularityGlobal = $this->createGranularity($organization, [], true, true);
-        $granularity11 = $this->createGranularity($organization, [$axis11], false);
-        $granularity12 = $this->createGranularity($organization, [$axis1, $axis2], true, false, true, false, true);
+        $granularityGlobal = $this->createGranularity($organization, [],                                                        true,  true,  true,  true,   true,  false, false, false);
+        $granularity_zone_marque = $this->createGranularity($organization, [$axis_zone, $axis_marque],                          true,  true,  true,  false,  true,  false, false, false);
+        $granularity_site = $this->createGranularity($organization, [$axis_site],                                               true,  false, true,  false,  true,  false, false, true );
+        $granularity_annee = $this->createGranularity($organization, [$axis_annee],                                             false, false, false, false,  false, false, false, false);
+        $granularity_annee_categorie = $this->createGranularity($organization, [$axis_annee, $axis_categorie],                  false, false, false, false,  false, false, false, false);
+        $granularity_annee_zone_marque = $this->createGranularity($organization, [$axis_annee, $axis_zone, $axis_marque],       false, false, false, false,  false, false, false, false);
+        $granularity_annee_site = $this->createGranularity($organization, [$axis_annee, $axis_site],                            false, false, false, false,  false, false, false, false);
+        $granularity_annee_site_categorie = $this->createGranularity($organization, [$axis_annee, $axis_site, $axis_categorie], false, false, false, false,  false, false, false, false);
 
         // Création des utilisateurs orga.
         // Params : email
-        $this->createUser('email1');
-        $this->createUser('email2');
-
-
+        $this->createUser('administrateur.organisation@toto.com');
+        $this->createUser('administrateur.global@toto.com');
+        $this->createUser('contributeur.global@toto.com');
+        $this->createUser('observateur.global@toto.com');
+        $this->createUser('administrateur.zone-marque@toto.com');
+        $this->createUser('contributeur.zone-marque@toto.com');
+        $this->createUser('observateur.zone-marque@toto.com');
+        $this->createUser('administrateur.site@toto.com');
+        $this->createUser('contributeur.site@toto.com');
+        $this->createUser('observateur.site@toto.com');
+        $this->createUser('acces.deux-sites@toto.com');
         $entityManager->flush();
 
 
         // Ajout d'un role sun une organisation à un utilisateur existant.
         // Params : email, Organization
-        $this->addOrganizationAdministrator('admin', $organization);
+        $this->addOrganizationAdministrator('administrateur.organisation@toto.com', $organization);
         // Ajout d'un role sur une cellule à un utilisateur existant.
         // Params : email, Granularity, [Member]
-        $this->addCellAdministrator('email1', $granularity11, [$member11a]);
-        $this->addCellContributor('email2', $granularity11, [$member11a]);
-        $this->addCellObserver('email1', $granularity12, [$member1a, $member2a]);
+        $this->addCellAdministrator('administrateur.zone-marque@toto.com', $granularity_zone_marque, [$member_zone_europe, $member_marque_marque_a]);
+        $this->addCellContributor('contributeur.zone-marque@toto.com', $granularity_zone_marque, [$member_zone_europe, $member_marque_marque_a]);
+        $this->addCellObserver('observateur.zone-marque@toto.com', $granularity_zone_marque, [$member_zone_europe, $member_marque_marque_a]);
 
 
         $entityManager->flush();
