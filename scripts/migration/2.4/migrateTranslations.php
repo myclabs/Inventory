99f440b1d836ede82195d4ee6e88dc47bab9744c
valentin-mcs <valentin.claras@myc-sense.com>
2013-07-17 15:45:14 +0200

    Modification du script pour pouvoir traduire n'importe quel attribut

--- scripts/migration/2.4/migrateTranslations.php
+++ scripts/migration/2.4/migrateTranslations.php
@@ -6,32 +6,36 @@ $entityManager = $entityManagers['default'];
 
 $temporaryLabel = 'Temporary Label';
 $translatableClasses = [
-    'Keyword_Model_Predicate' => [],
-    'Keyword_Model_Keyword' => [],
-    'Techno_Model_Category' => [],
-    'Techno_Model_Family' => [],
-    'Classif_Model_Axis' => [],
-    'Classif_Model_Member' => [],
-    'Classif_Model_Indicator' => [],
-    'Classif_Model_Context' => [],
-    'AF_Model_Category' => [],
-    'AF_Model_AF' => [],
-    'AF_Model_Component' => [],
-    'AF_Model_Component_Select_Option' => [],
-    'Algo_Model_Numeric' => [],
-    'Orga_Model_Organization' => [],
-    'Orga_Model_Axis' => [],
-    'Orga_Model_Member' => []
+    'Keyword_Model_Predicate' => ['attributes' => ['label', 'reverseLabel'], 'translations' => []],
+    'Keyword_Model_Keyword' => ['attributes' => ['label'], 'translations' => []],
+    'Techno_Model_Category' => ['attributes' => ['label'], 'translations' => []],
+    'Techno_Model_Family' => ['attributes' => ['label', 'documentation'], 'translations' => []],
+    'Techno_Model_Element' => ['attributes' => ['documentation'], 'translations' => []],
+    'Classif_Model_Axis' => ['attributes' => ['label'], 'translations' => []],
+    'Classif_Model_Member' => ['attributes' => ['label'], 'translations' => []],
+    'Classif_Model_Indicator' => ['attributes' => ['label'], 'translations' => []],
+    'Classif_Model_Context' => ['attributes' => ['label'], 'translations' => []],
+    'AF_Model_Category' => ['attributes' => ['label'], 'translations' => []],
+    'AF_Model_AF' => ['attributes' => ['label'], 'translations' => []],
+    'AF_Model_Component' => ['attributes' => ['label', 'help'], 'translations' => []],
+    'AF_Model_Component_Select_Option' => ['attributes' => ['label'], 'translations' => []],
+    'Algo_Model_Numeric' => ['attributes' => ['label'], 'translations' => []],
+    'Orga_Model_Organization' => ['attributes' => ['label'], 'translations' => []],
+    'Orga_Model_Axis' => ['attributes' => ['label'], 'translations' => []],
+    'Orga_Model_Member' => ['attributes' => ['label'], 'translations' => []]
 ];
 
 // Save old labels and set a temporary one.
-foreach ($translatableClasses as $className => $labels) {
-    echo "\n".'Saving old labels for '.$className."\n";
-    $listEntities = $className::loadList();
-    foreach ($listEntities as $entity) {
-        $translatableClasses[$className][spl_object_hash($entity)] = $entity->getLabel();
-        $entity->setLabel($temporaryLabel);
-        echo "\t".'from '.$translatableClasses[$className][spl_object_hash($entity)].' to '.$entity->getLabel()."\n";
+foreach ($translatableClasses as $className => $config) {
+    foreach ($config['attributes'] as $attribute) {
+        $setter = 'set'.ucfirst($attribute);
+        $getter = 'get'.ucfirst($attribute);
+        echo "\n".'Saving old "'.$attribute.'" for '.$className."\n";
+        foreach ($className::loadList() as $entity) {
+            $translatableClasses[$className]['translations'][$attribute][spl_object_hash($entity)] = $entity->$getter();
+            $entity->$setter($temporaryLabel);
+            echo "\t".'from '.$translatableClasses[$className]['translations'][$attribute][spl_object_hash($entity)].' to '.$entity->$getter()."\n";
+        }
     }
 }
 
@@ -40,12 +44,15 @@ $entityManager->flush();
 echo "\n".'-> Flush ended !'."\n";
 
 // Restore old labels.
-foreach ($translatableClasses as $className => $labels) {
-    echo "\n".'Restoring old labels for '.$className."\n";
-    $listEntities = $className::loadList();
-    foreach ($listEntities as $entity) {
-        echo "\t".'from '.$entity->getLabel().' to '.$translatableClasses[$className][spl_object_hash($entity)]."\n";
-        $entity->setLabel($translatableClasses[$className][spl_object_hash($entity)]);
+foreach ($translatableClasses as $className => $config) {
+    foreach ($config['attributes'] as $attribute) {
+        $setter = 'set'.ucfirst($attribute);
+        $getter = 'get'.ucfirst($attribute);
+        echo "\n".'Restoring old "'.$attribute.'" for '.$className."\n";
+        foreach ($className::loadList() as $entity) {
+            echo "\t".'from '.$entity->$getter().' to '.$translatableClasses[$className]['translations'][$attribute][spl_object_hash($entity)]."\n";
+            $entity->$setter($translatableClasses[$className]['translations'][$attribute][spl_object_hash($entity)]);
+        }
     }
 }
 
