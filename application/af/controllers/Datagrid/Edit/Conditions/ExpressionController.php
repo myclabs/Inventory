aa0809c5fed0ebb8622559866b535bfec744085b
Matthieu Napoli <matthieu@mnapoli.fr>
2013-07-25 09:35:04 +0200

    ref #6151 Exception non capturée

--- application/af/controllers/Datagrid/Edit/Conditions/ExpressionController.php
+++ application/af/controllers/Datagrid/Edit/Conditions/ExpressionController.php
@@ -121,7 +121,11 @@ class AF_Datagrid_Edit_Conditions_ExpressionController extends UI_Controller_Dat
                 break;
         }
         $condition->save();
-        $this->entityManager->flush();
+        try {
+            $this->entityManager->flush();
+        } catch (Core_ORM_DuplicateEntryException $e) {
+            throw new Core_Exception_User('UI', 'formValidation', 'alreadyUsedIdentifier');
+        }
         $this->message = __('UI', 'message', 'updated');
         $this->send();
     }
