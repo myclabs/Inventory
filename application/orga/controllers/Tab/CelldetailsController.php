8c60e3a40277caf2d081ba1f59737d7b81cef1a7
valentin-mcs <valentin.claras@myc-sense.com>
2013-07-31 08:31:38 +0200

    refs #6179

--- application/orga/controllers/Tab/CelldetailsController.php
+++ application/orga/controllers/Tab/CelldetailsController.php
@@ -334,8 +334,8 @@ class Orga_Tab_CelldetailsController extends Core_Controller
             }
         );
         foreach ($listInputGranularities as $inputGranularity) {
-            if ($cell->getGranularity()->isBroaderThan($inputGranularity->getInputConfigGranularity())
-                || ($cell->getGranularity()->getRef() === $inputGranularity->getInputConfigGranularity()->getRef())) {
+            if ($cell->getGranularity()->isBroaderThan($inputGranularity)
+                || ($cell->getGranularity()->getRef() === $inputGranularity->getRef())) {
                 $datagridConfiguration = new Orga_DatagridConfiguration(
                     'aFGranularity'.$idCell.'Input'.$inputGranularity->getId(),
                     'datagrid_cell_afgranularities_input',
