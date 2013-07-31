30a6f60d0ac109f7b6938b12eba4c4dfc3dfdf19
Matthieu Napoli <matthieu@mnapoli.fr>
2013-07-31 16:01:56 +0200

    ref #6205 La colonne "expression" de la table "Algo_Selection_Main" devrait être de type "text" et pas "varchar(255)"

--- scripts/migration/2.3/migrate.sql
+++ scripts/migration/2.3/migrate.sql
@@ -1,6 +1,14 @@
 # Valeur par défaut : true
-ALTER TABLE  `AF_InputSet_Primary`
+ALTER TABLE `AF_InputSet_Primary`
   ADD `calculationComplete` TINYINT( 1 ) NOT NULL DEFAULT '1';
 # Puis supprime la valeur par défaut
-ALTER TABLE  `AF_InputSet_Primary`
+ALTER TABLE `AF_InputSet_Primary`
   CHANGE `calculationComplete` `calculationComplete` TINYINT( 1 ) NOT NULL;
+
+# Corrige le type de la colonne
+ALTER TABLE `Algo_Selection_Main`
+  CHANGE `expression` `expression` LONGTEXT;
+# Puis restaure les expressions qui ont été tronquées
+UPDATE Algo_Selection_Main algo, TEC_Expression expression
+  SET algo.expression = expression.expression
+  WHERE expression.id = algo.idExpression
