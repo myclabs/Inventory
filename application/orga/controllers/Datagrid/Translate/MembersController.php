db21a81d6d1bc97ece448392a0681c6d8eaca7d9
valentin-mcs <valentin.claras@myc-sense.com>
2013-08-01 10:34:10 +0200

    refs #6208

--- application/orga/controllers/Datagrid/Translate/MembersController.php
+++ application/orga/controllers/Datagrid/Translate/MembersController.php
@@ -42,7 +42,11 @@ class Orga_Datagrid_Translate_MembersController extends UI_Controller_Datagrid
         foreach (Orga_Model_Member::loadList($this->request) as $member) {
             $data = array();
             $data['index'] = $member->getCompleteRef();
-            $data['identifier'] = $member->getAxis()->getRef().' | '.$member->getCompleteRef();
+            $data['identifier'] = $member->getAxis()->getRef().' | '.$member->getRef();
+            $parentMembersHshKey = $member->getParentMembersHashKey();
+            if (!empty($parentMembersHshKey)) {
+                $data['identifier'] .= ' ('. $parentMembersHshKey .')';
+            }
 
             foreach (Zend_Registry::get('languages') as $language) {
                 $locale = Core_Locale::load($language);
