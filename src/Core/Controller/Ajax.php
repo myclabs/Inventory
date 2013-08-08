c87337f3e1dfebe99040c32df298d7f274a54884
Matthieu Napoli <matthieu@mnapoli.fr>
2013-08-08 15:59:54 +0200

    Revert d'une modif qui désactivait le plugin flush

--- src/Core/Controller/Ajax.php
+++ src/Core/Controller/Ajax.php
@@ -22,11 +22,8 @@ abstract class Core_Controller_Ajax extends Core_Controller
         // Toute cette manipulation est nécessaire pour contourner
         //  un bug de Zend Framework (les headers firebug ne sont pas envoyés sinon).
         //@see http://framework.zend.com/issues/browse/ZF-4134
-
-        /** @var Zend_Controller_Action_Helper_Json $json */
         $json = $this->getHelper('Json');
-        // Commenté suite à http://dev.myc-sense.com:3000/issues/6251
-//        $json->suppressExit = true;
+        $json->suppressExit = true;
         $json->sendJson($reponse);
         Zend_Wildfire_Channel_HttpHeaders::getInstance()->flush();
     }
