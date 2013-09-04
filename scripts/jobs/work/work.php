b7f3d8ff2516ced64160d1dbfe1b306ea8c08375
Matthieu Napoli <matthieu@mnapoli.fr>
2013-09-04 11:34:30 +0200

    feature ref ##6275 Correction du log pour les workers

--- scripts/jobs/work/work.php
+++ scripts/jobs/work/work.php
@@ -8,6 +8,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Processor\PsrLogMessageProcessor;
 
+define('APPLICATION_ENV', 'script');
 define('RUN', false);
 
 require_once __DIR__ . '/../../../application/init.php';
