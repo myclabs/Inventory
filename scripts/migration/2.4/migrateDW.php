d463d17883eb70351b7c79c1cae4ab39fafbbed6
valentin-mcs <valentin.claras@myc-sense.com>
2013-09-05 09:31:17 +0200

    refs #6401

--- scripts/migration/2.4/migrateDW.php
+++ scripts/migration/2.4/migrateDW.php
@@ -5,23 +5,23 @@ $entityManagers = Zend_Registry::get('EntityManagers');
 $entityManager = $entityManagers['default'];
 
 foreach (DW_Model_Indicator::loadList() as $indicator) {
-    if (strpos($indicator->getRef(), 'classif_')) {
+    if (strpos($indicator->getRef(), 'classif_') === 0) {
         $indicator->setRef(substr($indicator->getRef(), 8));
     }
 }
 
 foreach (DW_Model_Axis::loadList() as $axis) {
-    if (strpos($axis->getRef(), 'classif_')) {
+    if (strpos($axis->getRef(), 'classif_') === 0) {
         $axis->setRef('c_'.substr($axis->getRef(), 8));
-    } elseif (strpos($axis->getRef(), 'orga_')) {
+    } elseif (strpos($axis->getRef(), 'orga_') === 0) {
         $axis->setRef('o_'.substr($axis->getRef(), 5));
     }
 }
 
 foreach (DW_Model_Member::loadList() as $member) {
-    if (strpos($member->getRef(), 'classif_')) {
+    if (strpos($member->getRef(), 'classif_') === 0) {
         $member->setRef(substr($member->getRef(), 8));
-    } elseif (strpos($member->getRef(), 'orga_')) {
+    } elseif (strpos($member->getRef(), 'orga_') === 0) {
         $member->setRef(substr($member->getRef(), 5));
     }
 }
