025cd1f0c390a283aa998870902803d4518c0ab2
valentin-mcs <valentin.claras@myc-sense.com>
2013-11-13 11:43:26 +0100

    Amélioration du script de migration

--- scripts/migration/2.9/migrateOrga.php
+++ scripts/migration/2.9/migrateOrga.php
@@ -4,14 +4,15 @@ $entityManagers = Zend_Registry::get('EntityManagers');
 /** @var $entityManager \Doctrine\ORM\EntityManager */
 $entityManager = $entityManagers['default'];
 
+echo "\n".'Ajout des Tag aux éléments de la structure organisationnelle…'."\n";
 /** @var Orga_Model_Organization $organization */
 foreach (Orga_Model_Organization::loadList() as $organization) {
-    echo "Root axes" . PHP_EOL;
+    echo "\tRoot axes" . PHP_EOL;
     /** @var Orga_Model_Axis $rootAxis */
     foreach ($organization->getRootAxes() as $rootAxis) {
         $rootAxis->updateNarrowerTag();
     }
-    echo "Axes & members" . PHP_EOL;
+    echo "\tAxes & members" . PHP_EOL;
     $axes = $organization->getAxes()->toArray();
     $axes = array_reverse($axes);
     /** @var Orga_Model_Axis $axis */
@@ -22,23 +23,178 @@ foreach (Orga_Model_Organization::loadList() as $organization) {
             $member->updateTag();
         }
     }
-    echo "Granularities" . PHP_EOL;
+    echo "\tGranularities" . PHP_EOL;
     foreach ($organization->getGranularities() as $granularity) {
         $granularity->updateRef();
         $granularity->updateTag();
     }
-    echo "Cells" . PHP_EOL;
-    foreach ($axes as $axis) {
-        $axis->updateBroaderTag();
-        foreach ($axis->getMembers() as $member) {
-            foreach ($member->getCells() as $cell) {
-                $cell->updateMembersHashKey();
-                $cell->updateTags();
-            }
+    echo "\tCells" . PHP_EOL;
+    foreach ($organization->getGranularities() as $granularity) {
+        foreach ($granularity->getCells() as $cell) {
+            $cell->updateMembersHashKey();
+            $cell->updateTag();
+        }
+    }
+    foreach ($organization->getGranularities() as $granularity) {
+        foreach ($granularity->getCells() as $cell) {
+            $cell->updateHierarchy();
         }
     }
 }
+echo "\n".'…ajout terminé !'."\n";
 
 echo "\n".'-> Flush starting…'."\n";
 $entityManager->flush();
 echo "\n".'-> Flush ended !'."\n";
+
+echo "\n".'Vérification de la structure…'."\n";
+$wrongNarrowerTagAxes = [];
+$wrongBroaderTagAxes = [];
+$wrongTagMembers = [];
+$wrongTagGranularities = [];
+$wrongTagCells = [];
+foreach (Orga_Model_Organization::loadList() as $organization) {
+    foreach ($organization->getAxes() as $axis) {
+        $expectedNarrowerTag = buildAxisNarrowerTag($axis);
+        if ($expectedNarrowerTag !== $axis->getNarrowerTag()) {
+            $wrongNarrowerTagAxes[] = [$axis, $expectedNarrowerTag];
+        }
+        $expectedBroaderTag = buildAxisBroaderTag($axis);
+        if ($expectedBroaderTag !== $axis->getBroaderTag()) {
+            $wrongBroaderTagAxes[] = [$axis, $expectedBroaderTag];
+        }
+    }
+    foreach ($organization->getAxes() as $axis) {
+        foreach ($axis->getMembers() as $member) {
+            $expectedTag = buildMemberTag($member);
+            if ($expectedTag !== $member->getTag()) {
+                $wrongTagMembers[] = [$member, $expectedTag];
+            }
+        }
+    }
+    foreach ($organization->getGranularities() as $granularity) {
+        $expectedTag = buildGranularityTag($granularity);
+        if ($expectedTag !== $granularity->getTag()) {
+            $wrongTagGranularities[] = [$granularity, $expectedTag];
+        }
+    }
+    foreach ($organization->getGranularities() as $granularity) {
+        foreach ($granularity->getCells() as $cell) {
+            $expectedTag = buildCellTag($cell);
+            if ($expectedTag !== $cell->getTag()) {
+                $wrongTagCells[] = [$cell, $expectedTag];
+            }
+        }
+    }
+}
+echo "\n".'…vérification terminé !'."\n";
+echo "\n".'Affichage des erreurs :'."\n";
+if (!empty($wrongNarrowerTagAxes)) {
+    echo "\t Wrong Narrower Tag Axes :" . PHP_EOL;
+    foreach ($wrongNarrowerTagAxes as $errorArray) {
+        $axis = $errorArray[0];
+        echo "\t\t".$axis->getId()." - ".$axis->getLabel()." : ".$axis->getNarrowerTag()." expected ".$errorArray[1]."\n";
+    }
+}
+if (!empty($wrongBroaderTagAxes)) {
+    echo "\t Wrong Broader Tag Axes :" . PHP_EOL;
+    foreach ($wrongBroaderTagAxes as $errorArray) {
+        $axis = $errorArray[0];
+        echo "\t\t".$axis->getId()." - ".$axis->getLabel()." : ".$axis->getBroaderTag()." expected ".$errorArray[1]."\n";
+    }
+}
+if (!empty($wrongTagMembers)) {
+    echo "\t Wrong Tag Members :" . PHP_EOL;
+    foreach ($wrongTagMembers as $errorArray) {
+        $member = $errorArray[0];
+        echo "\t\t".$member->getId()." - ".$member->getLabel()." : ".$member->getTag()." expected ".$errorArray[1]."\n";
+    }
+}
+if (!empty($wrongTagGranularities)) {
+    echo "\t Wrong Tag Granularities :" . PHP_EOL;
+    foreach ($wrongTagGranularities as $errorArray) {
+        $granularity = $errorArray[0];
+        echo "\t\t".$granularity->getId()." - ".$granularity->getLabel()." : ".$granularity->getTag()." expected ".$errorArray[1]."\n";
+    }
+}
+if (!empty($wrongTagMembers)) {
+    echo "\t Wrong Tag Cells :" . PHP_EOL;
+    foreach ($wrongTagMembers as $errorArray) {
+        $cell = $errorArray[0];
+        echo "\t\t".$cell->getId()." - ".$cell->getLabel()." : ".$cell->getTag()." expected ".$errorArray[1]."\n";
+    }
+}
+echo "\n".'Migration terminée !'."\n";
+
+function buildAxisNarrowerTag (Orga_Model_Axis $axis) {
+    $narrowerTag = Orga_Model_Organization::PATH_SEPARATOR;
+    $directNarrower = $axis->getDirectNarrower();
+    while ($directNarrower != null) {
+        $narrowerTag = Orga_Model_Organization::PATH_SEPARATOR. $directNarrower->getAxisTag() . $narrowerTag;
+        $directNarrower = $directNarrower->getDirectNarrower();
+    }
+    return $narrowerTag . $axis->getAxisTag() . Orga_Model_Organization::PATH_SEPARATOR;
+}
+
+function buildAxisBroaderTag (Orga_Model_Axis $axis) {
+    if ($axis->hasDirectBroaders()) {
+        $broaderTag = '';
+        foreach ($axis->getDirectBroaders() as $directBroader) {
+            $directBroaderTag = buildAxisBroaderTag($directBroader);
+            foreach (explode(Orga_Model_Organization::PATH_JOIN, $directBroaderTag) as  $pathTag) {
+                $broaderTag .= $pathTag . $axis->getAxisTag() . Orga_Model_Organization::PATH_SEPARATOR . Orga_Model_Organization::PATH_JOIN;
+            }
+        }
+        $broaderTag = substr($broaderTag, 0, -1);
+    } else {
+        $broaderTag = Orga_Model_Organization::PATH_SEPARATOR . $axis->getAxisTag() . Orga_Model_Organization::PATH_SEPARATOR;
+    }
+    return $broaderTag;
+}
+
+function buildMemberTag (Orga_Model_Member $member) {
+    if ($member->hasDirectParents()) {
+        $tag = '';
+        foreach ($member->getDirectParents() as $directParent) {
+            $directParentTag = buildMemberTag($directParent);
+            foreach (explode(Orga_Model_Organization::PATH_JOIN, $directParentTag) as  $pathTag) {
+                $tag .= $pathTag . $member->getMemberTag() . Orga_Model_Organization::PATH_SEPARATOR . Orga_Model_Organization::PATH_JOIN;
+            }
+        }
+        $tag = substr($tag, 0, -1);
+    } else {
+        $tag = Orga_Model_Organization::PATH_SEPARATOR . $member->getMemberTag() . Orga_Model_Organization::PATH_SEPARATOR;
+    }
+    return $tag;
+}
+
+function buildGranularityTag (Orga_Model_Granularity $granularity) {
+    if ($granularity->hasAxes()) {
+        $tag = '';
+        $axes = $granularity->getAxes();
+        @usort($axes, ['Orga_Model_Axis', 'firstOrderAxes']);
+        $axes = array_reverse($axes);
+        foreach ($axes as $axis) {
+            $tag .= buildAxisBroaderTag($axis) . Orga_Model_Organization::PATH_JOIN;
+        }
+        $tag = substr($tag, 0, -1);
+    } else {
+        $tag = Orga_Model_Organization::PATH_SEPARATOR;
+    }
+    return $tag;
+}
+
+function buildCellTag (Orga_Model_Cell $cell) {
+    if ($cell->hasMembers()) {
+        $tag = '';
+        $members = $cell->getMembers();
+        @usort($members, ['Orga_Model_Member', 'orderMembers']);
+        foreach ($members as $member) {
+            $tag .= buildMemberTag($member) . Orga_Model_Organization::PATH_JOIN;
+        }
+        $tag = substr($tag, 0, -1);
+    } else {
+        $tag = Orga_Model_Organization::PATH_SEPARATOR;
+    }
+    return $tag;
+}
