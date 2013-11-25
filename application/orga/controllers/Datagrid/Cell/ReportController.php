6a81319b8a536e8dbd3075b876b0b891c6b8c192
Matthieu Napoli <matthieu@mnapoli.fr>
2013-11-25 11:12:53 +0100

    bug ref #6703 Les analyses préconfigurées ne sont plus visibles au niveau des cellules

--- application/orga/controllers/Datagrid/Cell/ReportController.php
+++ application/orga/controllers/Datagrid/Cell/ReportController.php
@@ -29,13 +29,32 @@ class Orga_Datagrid_Cell_ReportController extends UI_Controller_Datagrid
      */
     public function getelementsAction()
     {
+        // Rapports préconfigurés
+        $cube = DW_Model_Cube::load($this->getParam('idCube'));
+        foreach ($cube->getReports() as $report) {
+            if (! Orga_Model_GranularityReport::isDWReportCopiedFromGranularityDWReport($report)) {
+                continue;
+            }
+
+            $data = array();
+            $data['index'] = $report->getId();
+            $data['label'] = $report->getLabel();
+            $urlDetails = 'orga/tab_celldetails/report/idCell/'.$this->getParam('idCell').'/idReport/'.$data['index'];
+            $data['details'] = $this->cellLink($urlDetails);
+            $data['delete'] = false;
+
+            $this->addline($data);
+        }
+
+        // Rapports personnels
         $this->request->aclFilter->enabled = true;
         $this->request->aclFilter->user = $this->_helper->auth();
         $this->request->aclFilter->action = Action::VIEW();
 
-        $this->request->filter->addCondition(DW_Model_Report::QUERY_CUBE, DW_Model_Cube::load($this->getParam('idCube')));
+        $this->request->filter->addCondition(DW_Model_Report::QUERY_CUBE, $cube);
         $this->request->order->addOrder(DW_Model_Report::QUERY_LABEL);
         foreach (DW_Model_Report::loadList($this->request) as $report) {
+            /** @var DW_Model_Report $report */
             $data = array();
             $data['index'] = $report->getId();
             $data['label'] = $report->getLabel();
