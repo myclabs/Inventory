83746af4d301e6488caf962e95c504b7064f308e
Matthieu Napoli <matthieu@mnapoli.fr>
2013-12-06 10:48:46 +0100

    ref #6770 Refactoring Unit : suppression des vues des unités composées et étendues

--- src/Unit/Application/Controller/TranslateController.php
+++ src/Unit/Application/Controller/TranslateController.php
@@ -1,24 +1,13 @@
 <?php
-/**
- * @author     matthieu.napoli
- * @package    Techno
- * @subpackage Controller
- */
 
 use Core\Annotation\Secure;
 
-/**
- * @package    Techno
- * @subpackage Controller
- */
 class Unit_TranslateController extends Core_Controller
 {
-
     /**
      * @Secure("viewUnit")
      */
     public function translationsAction()
     {
     }
-
 }
