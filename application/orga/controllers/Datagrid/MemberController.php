d2c340a1caef990209264fa1143b0fb742dafec5
valentin-mcs <valentin.claras@myc-sense.com>
2013-12-10 16:30:32 +0100

    Amélioration des méthodes d'Orga pour éviter de chainer inutilement des criteria. Suppression de controller inutiles. Amélioration de la vue et du CellViewModel

--- application/orga/controllers/Datagrid/MemberController.php
+++ application/orga/controllers/Datagrid/MemberController.php
@@ -260,7 +260,7 @@ class Orga_Datagrid_MemberController extends UI_Controller_Datagrid
         $organization = Orga_Model_Organization::load($this->getParam('idOrganization'));
         $broaderAxis = $organization->getAxisByRef($this->getParam('refParentAxis'));
 
-        $members = $broaderAxis->getMembers()->toArray();
+        $members = $broaderAxis->getOrderedMembers()->toArray();
         $idCell = $this->getParam('idCell');
         if (!empty($idCell)) {
             $cell = Orga_Model_Cell::load($idCell);
