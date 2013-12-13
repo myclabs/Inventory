a0f6fc385dc01648559a09af6c817189fc0bc0c6
valentin-mcs <valentin.claras@myc-sense.com>
2013-12-13 15:41:27 +0100

    Correction du script de migration

--- scripts/migration/2.10/migrate.php
+++ scripts/migration/2.10/migrate.php
@@ -7,6 +7,11 @@ define('RUN', false);
 
 require_once __DIR__ . '/../../../application/init.php';
 
+/** @var \DI\Container $container */
+$container = Zend_Registry::get('container');
+/** @var EntityManager $em */
+$em = $container->get(EntityManager::class);
+
 // Run build update to update DB
 echo "Executing build update" . PHP_EOL . PHP_EOL;
 $output = [];
