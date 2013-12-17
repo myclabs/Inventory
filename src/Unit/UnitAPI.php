8d0e52a288b82f17a152534f52d4f764742024fd
Matthieu Napoli <matthieu@mnapoli.fr>
2013-12-17 11:49:00 +0100

    ref #6770 Refactoring Unit : MAJ de UnitAPI pour utiliser les webservices

--- src/Unit/UnitAPI.php
+++ src/Unit/UnitAPI.php
@@ -3,25 +3,26 @@
 namespace Unit;
 
 use DI\Container;
+use MyCLabs\UnitAPI\DTO\UnitDTO;
 use MyCLabs\UnitAPI\Exception\IncompatibleUnitsException;
 use MyCLabs\UnitAPI\Exception\UnknownUnitException;
 use MyCLabs\UnitAPI\UnitOperationService;
 use MyCLabs\UnitAPI\UnitService;
 
 /**
- * API
- * @author     valentin.claras
- * @author     hugo.charbonnier
- * @author     yoann.croizer
+ * API pour utiliser les unités.
+ *
+ * @author valentin.claras
+ * @author hugo.charbonnier
+ * @author yoann.croizer
  */
 class UnitAPI
 {
     /**
-     * Référent textuel d'une unité
-     *  si le référent contient un "." c'et qu'il s'agit d'une unité composée.
+     * Identifiant d'une unité
      * @var string
      */
-    protected $ref;
+    private $ref;
 
     /**
      * @param string $ref
@@ -63,6 +64,7 @@ class UnitAPI
 
     /**
      * Renvoi le référent textuel de l'API.
+     *
      * @return string
      */
     public function getRef()
@@ -71,12 +73,14 @@ class UnitAPI
     }
 
     /**
-     * getSymbol()
      * Va prendre en paramètre le référent textuel d'une unité et va retourner le symbol de l'unité
      * sous forme de chaîne de caractères. La variable booléenne $html si elle est à "true" permet de
      * transformer par exemple un exposant de la forme m^2 en m<sup>2</sup>.
      *
-     * @return String
+     * @todo À mettre en cache
+     *
+     * @throws UnknownUnitException
+     * @return string
      */
     public function getSymbol()
     {
@@ -93,7 +97,10 @@ class UnitAPI
 
     /**
      * Vérifie qu'une ref est compatible avec l'unité.
+     *
      * @param string $ref
+     *
+     * @throws UnknownUnitException
      * @return bool
      */
     public function isEquivalent($ref)
@@ -113,6 +120,8 @@ class UnitAPI
      *     sont équivalentes)
      *
      * @param string $refUnit
+     *
+     * @throws UnknownUnitException
      * @throws IncompatibleUnitsException
      * @return float Le facteur de conversion.
      */
@@ -130,7 +139,10 @@ class UnitAPI
      * Sert à multiplier des unités. Renvoie une chaine de caractère qui
      *  correspond à une unité composéé d'unités de référence de grandeur
      *   physique de base (univoque).
+     *
      * @param array $components
+     *
+     * @throws UnknownUnitException
      * @return UnitAPI $api
      */
     public static function multiply($components)
@@ -144,7 +156,10 @@ class UnitAPI
     /**
      * Sert à ajouter des unités entre elles. Renvoi une unité composée
      *  d'unités de référence de grandeur physique de base.
+     *
      * @param array $components
+     *
+     * @throws UnknownUnitException
      * @return UnitAPI
      */
     public static function calculateSum($components)
@@ -157,23 +172,29 @@ class UnitAPI
 
     /**
      * Renvoie la liste des refs des unités compatibles, càd de même grandeur physique.
+     *
+     * @throws UnknownUnitException
      * @return UnitAPI[]
      */
     public function getCompatibleUnits()
     {
-        $unit = new ComposedUnit($this->getRef());
+        /** @var Container $container */
+        $container = \Zend_Registry::get('container');
+        /** @var UnitService $unitService */
+        $unitService = $container->get(UnitService::class);
 
-        return array_map(
-            function (ComposedUnit $unit) {
-                return new UnitAPI($unit->getRef());
-            },
-            $unit->getCompatibleUnits()
-        );
+        $unitDTOs = $unitService->getCompatibleUnits($this->ref);
+
+        return array_map(function (UnitDTO $unitDTO) {
+            return new UnitAPI($unitDTO->id);
+        }, $unitDTOs);
     }
 
 
     /**
      * Renvoie l'unité normalisée associée à une unité.
+     *
+     * @throws UnknownUnitException
      * @return UnitAPI
      */
     public function getNormalizedUnit()
@@ -183,13 +204,18 @@ class UnitAPI
     }
 
     /**
-     * Retourne l'inverse de l'unité
+     * Retourne l'inverse de l'unité.
+     *
+     * @throws UnknownUnitException
      * @return UnitAPI
      */
     public function reverse()
     {
-        $composedUnit = new ComposedUnit($this->getRef());
-        $composedUnit->reverseUnit();
-        return new UnitAPI($composedUnit->getRef());
+        /** @var Container $container */
+        $container = \Zend_Registry::get('container');
+        /** @var UnitOperationService $operationService */
+        $operationService = $container->get(UnitOperationService::class);
+
+        return new UnitAPI($operationService->inverse($this->ref));
     }
 }
