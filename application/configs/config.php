637d26b18860130a48e46c3f6b0d34b5734ba054
Matthieu Napoli <matthieu@mnapoli.fr>
2014-01-13 11:25:25 +0100

    Refactoring des scripts, MAJ de PHP-DI et suppression de tous les appels à Zend_Registry

--- application/configs/config.php
+++ application/configs/config.php
@@ -1,8 +1,5 @@
 <?php
 
-use DI\Container;
-use Psr\Log\LoggerInterface;
-use User\Domain\ACL\ACLService;
 use User\Domain\UserService;
 
 return [
@@ -19,20 +16,10 @@ return [
     'emails.noreply.name'   => 'My C-Tool',
     'emails.noreply.adress' => 'noreply@myc-sense.com',
     'emails.contact.adress' => 'contact@myc-sense.com',
-    // @see https://github.com/mnapoli/PHP-DI/issues/144
-//    UserService::class      => DI\object()
-//            ->methodParameter('__construct', 'contactEmail', DI\link('emails.contact.adress'))
-//            ->methodParameter('__construct', 'noReplyEmail', DI\link('emails.noreply.adress'))
-//            ->methodParameter('__construct', 'noReplyName', DI\link('emails.noreply.name')),
-    UserService::class      => DI\factory(function (Container $c) {
-        return new UserService(
-            $c->get(ACLService::class),
-            $c->get(LoggerInterface::class),
-            $c->get('emails.contact.adress'),
-            $c->get('emails.noreply.adress'),
-            $c->get('emails.noreply.name')
-        );
-    }),
+    UserService::class      => DI\object()
+            ->methodParameter('__construct', 'contactEmail', DI\link('emails.contact.adress'))
+            ->methodParameter('__construct', 'noReplyEmail', DI\link('emails.noreply.adress'))
+            ->methodParameter('__construct', 'noReplyName', DI\link('emails.noreply.name')),
 
     // Chemin vers les fichier de fonts (nécéssaire pour le Captcha)
     'police.path' => 'data/fonts/',
