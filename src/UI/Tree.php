643ff7517cb0d7aedd32c4a9747f43724e19397c
Matthieu Napoli <matthieu@mnapoli.fr>
2014-05-28 12:10:42 +0200

    bug ref #7139

--- src/UI/Tree.php
+++ src/UI/Tree.php
@@ -1091,6 +1091,7 @@ class UI_Tree extends UI_Generic
         $broker->view->headScript()->appendFile('yui/build/connection/connection-min.js', 'text/javascript');
         $broker->view->headScript()->appendFile('yui/build/json/json-min.js', 'text/javascript');
 
+        $broker->view->headScript()->appendFile('scripts/ui/form-ajax.js', 'text/javascript');
         $broker->view->headScript()->appendFile('scripts/ui/form-action.js', 'text/javascript');
         $broker->view->headLink()->appendStylesheet('markitup/skins/markitup/style.css');
         $broker->view->headLink()->appendStylesheet('markitup/skins/textile/style.css');
